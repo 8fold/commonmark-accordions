1eba8c3a0978ae279b4695acd337f22be0368382
Josh Bruce <josh@8fold.pro>
2020-07-30 16:04:44 -0500

    Update ef-accordions.js

--- src/js/ef-accordions.js
+++ src/js/ef-accordions.js
@@ -1,15 +1,59 @@
+var efAccordionGroup = {
+  initialized: false,
+  closeAll: function(g) {
+    Array.from(g.querySelectorAll('[is="accordion"]')).forEach(a => {
+      b = a.querySelector('button:first-of-type');
+      expanded = b.getAttribute("aria-expanded") === 'true';
+      if (expanded) {
+        b.setAttribute("aria-expanded","false");
+        b.parentElement.nextElementSibling.setAttribute("aria-hidden","true");
+      }
+    });
+  }
+}
+
+let efCloseAccordion = function(b) {
+  expanded = b.getAttribute("aria-expanded") === 'true';
+  if (expanded) {
+    b.setAttribute("aria-expanded","false");
+    b.parentElement.nextElementSibling.setAttribute("aria-hidden","true");
+  }
+}
+
+let efOpenAccordion = function(b) {
+  expanded = b.getAttribute("aria-expanded") === 'true';
+  if (!expanded) {
+    g = b.parentElement.parentElement;
+    if (g.hasAttribute('is') && g.getAttribute('is') === 'accordion-group') {
+      efAccordionGroup.closeAll(g);
+    }
+    b.setAttribute("aria-expanded","true");
+    p = b.parentElement.nextElementSibling
+    p.setAttribute("aria-hidden","false");
+    p.focus();
+  }
+}
+
 let efToggleAccordion = function(e) {
-  e.preventDefault();
-  b = e.target;
-  p = b.parentElement.nextElementSibling;
-  e = (b.getAttribute("aria-expanded") === 'true');
-  b.setAttribute("aria-expanded", (!e).toString());
-  p.setAttribute("aria-hidden", e.toString());
-  if (e) { p.focus(); }
+  if (e !== undefined) {
+    e.preventDefault();
+    b = e.target;
+    expanded = b.getAttribute("aria-expanded") === 'true';
+    if (expanded) {
+      efCloseAccordion(b);
+    } else {
+      efOpenAccordion(b);
+    }
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
   Array.from(document.querySelectorAll('[is="accordion"]')).forEach(a => {
-    efToggleAccordion(a.querySelector('button:first-of-type'));
+    b = a.querySelector('button:first-of-type');
+    if (a.parentElement.hasAttribute('is') && a.parentElement.getAttribute('is') === 'accordion-group') {
+      efAccordionGroup.closeAll(a.parentElement);
+    } else {
+      efCloseAccordion(b);
+    }
   });
 });
